bfe6ecad372527dc819befaa0bd5626e36dff91b
Fozzy Yeti <christopherrclark@gmail.com>
2023-02-12 15:18:02 -0600

    changed info in seeds folders, added and filled out .env file

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -1,19 +1,19 @@
 const Post = require('../models/Post');
 
 const postsSeeds = [
-    //Clay's post
+    //Bob's post
     {
         user_id: 1, 
         post_date: "01/01/2022",
-        post_title: "Clay's post title!",
-        post_content: "This is the body of Clay's post!!"
+        post_title: "Bob's post title!",
+        post_content: "This is the body of Bob's post!!"
     },
     //Mike's post
     {
         user_id: 2, 
         post_date: "01/01/2022",
-        post_title: "Mike's post title!",
-        post_content: "This is the body of Mike's post!!"
+        post_title: "Jason's post title!",
+        post_content: "This is the body of Jason's post!!"
     },
 ];
 
