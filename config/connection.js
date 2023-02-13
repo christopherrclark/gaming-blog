23e74337f96965aa214091fc246303d8f71c7cf3
Kenny Cordero <GeckoB4I@gmail.com>
2023-02-13 12:54:53 -0600

    sutff?

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+//^Tutor helped me set this up
+const sequelize = process.env.MYSQL_URL
+  ? new Sequelize(process.env.MYSQL_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+
+module.exports = sequelize;
+
+
 // let sequelize;
 
 // if (process.env.MYSQL_URL) {
