11172f5197f1834c2d330ec2ac13fc8cda25d07d
clayandemar <clayandemar@gmail.com>
2023-02-14 10:18:42 -0600

    api

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -117,7 +117,8 @@ router.get('/login', async (req, res) => {
   }
 });
 
-// API call
+
+// API call and setup
 // GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
 // GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
 
